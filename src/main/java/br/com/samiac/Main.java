8044a6e48a1f708d4c866e83cd48b5716cf8c6c9
Samuel Teixeira de Souza <samuelsouza.dev@outlook.com>
2026-01-18 13:20:15 -0300

    feat: add CountingValleys solution

--- src/main/java/br/com/samiac/Main.java
+++ src/main/java/br/com/samiac/Main.java
@@ -1,11 +1,12 @@
 package br.com.samiac;
 
+import br.com.samiac.problems.hackerrank.CountingValleys;
 import br.com.samiac.problems.hackerrank.ElectronicsShop;
 import br.com.samiac.problems.leetcode.SearchInsertPosition;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(ElectronicsShop.getMoneySpent(new int[]{3, 1}, new int[]{5, 2, 8}, 10));
+		System.out.println(CountingValleys.countingValleys(8, "UDDDUDUU"));
 	}
 }

3ec3b62b145b5614598ee98169371fdd365d517e
sameldevt <samuelsouza.dev@outlook.com>
2026-05-29 08:38:41 -0300

    feat(leetcode): add Contains Duplicate solution

---
+++ src/main/java/br/com/samiac/problems/leetcode/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package br.com.samiac.problems.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int n : nums){
+            if(!set.add(n)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

d588b764434d8f4b3907281e6efccfdaf9c232c3
sameldevt <samuelsouza.dev@outlook.com>
2026-09-14 07:49:12 -0300

    feat(leetcode): add 'Power of Two' solution

---
+++ src/main/java/br/com/samiac/problems/leetcode/PowerOfTwo.java
@@ -0,0 +1,16 @@
+package br.com.samiac.problems.leetcode;
+
+public class PowerOfTwo {
+
+	public boolean isPowerOfTwo(int n) {
+		if (n == 1) {
+			return true;
+		}
+
+		if (n <= 0 || n % 2 != 0) {
+			return false;
+		}
+
+		return isPowerOfTwo(n / 2);
+	}
+}
